2dc1694070faca001d399b68a34c65abe0c8116b
Tyler Koenig <tylerkng96@icloud.com>
2021-10-09 12:39:03 -0400

    installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import './App.scss';
 import Header from './components/Header';
 import TaskManager from './components/TaskManager';
 import ExpenseTracker from './components/expense/ExpenseTracker';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
 	return (
-		<div className='wrapper'>
-			<Header />
-			<TaskManager />
-			<ExpenseTracker />
-		</div>
+		<Router>
+			<div className='wrapper'>
+				<Header />
+				<div className='content'>
+					<Switch>
+						<Route path='/'></Route>
+						<Route path='/todolist'>
+							<TaskManager />
+						</Route>
+					</Switch>
+				</div>
+				<ExpenseTracker />
+			</div>
+		</Router>
 	);
 }
 
