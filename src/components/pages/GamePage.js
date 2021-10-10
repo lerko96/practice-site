26b458f50bf252e9b98561131dbde6979c2a7b7b
Tyler Koenig <tylerkng96@icloud.com>
2021-10-10 11:49:29 -0400

    routing path configuration

--- src/components/pages/GamePage.js
+++ src/components/pages/GamePage.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import { useRouteMatch } from 'react-router';
 import { Switch, Route, Link } from 'react-router-dom';
+import TaskManager from '../tools/toDoList/TaskManager';
 
-const Game = ({ match }) => <p>{match.params.id}</p>
+const Game = ({ match }) => <p>{match.params.id}</p>;
 
 class GamePage extends React.Component {
 	render() {
-        const { url } = this.props.match
-        return (
-		<div className='game-page'>
-			<h1>Games</h1>
-			<div>
-                <Link to='/games/donut-clicker'>Game 1</Link>
+		const { url } = this.props.match;
+		return (
+			<div className='game-page'>
+				<h1>Games</h1>
+				<div>
+					<Link to='/games/donut-clicker'>Game 1</Link>
 
-				<Link to='#'>Fake Game 2</Link>
+					<Link to='#'>Fake Game 2</Link>
+				</div>
+
+				<Route path='/games/:id' component={TaskManager} />
 			</div>
-		</div>
-
-        )
-    }
-};
+		);
+	}
+}
 
 export default GamePage;
