f466d26b35d626b32466386ca5d9b3c0305f34a3
Tyler Koenig <tylerkng96@icloud.com>
2021-10-11 22:54:04 -0400

    redesign home, added search function

---
+++ src/components/Search.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+const Search = () => {
+	const [gSearch, setGSearch] = useState('');
+
+	// const handleSubmit = (e) => {};
+	const handleSearch = () => {
+		window.open('http://google.com/search?q=' + gSearch);
+	};
+
+	return (
+		<div className='search-container'>
+			<div className='search-bar'>
+				<form>
+					<button id='search' onClick={handleSearch}>
+						<i class='fas fa-otter'></i>
+					</button>
+					<input
+						type='text'
+						autoFocus
+						required
+						placeholder='search the internet'
+						value={gSearch}
+						onChange={(e) => setGSearch(e.target.value)}
+					/>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default Search;
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Weather from '../tools/weather/Weather.js';
 import ProjectList from '../ProjectList';
 import ToolsList from './ToolPage';
+import Search from '../Search.js';
 
 const HomePage = () => {
 	const [name, setName] = useState('lerko');
@@ -10,57 +11,54 @@ const HomePage = () => {
 		setName('luigi');
 	};
 
-	const [lat, setLat] = useState([]);
-	const [long, setLong] = useState([]);
-	const [quote, setQuote] = useState([]);
-	const [data, setData] = useState([]);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			navigator.geolocation.getCurrentPosition(function (position) {
-				setLat(position.coords.latitude);
-				setLong(position.coords.longitude);
-			});
 
-			await fetch('https://quotes.rest/qod')
-				.then((res) => res.json())
-				.then((resultQuote) => {
-					setData(resultQuote);
-					console.log(resultQuote);
-				});
+	// const [lat, setLat] = useState([]);
+	// const [long, setLong] = useState([]);
+	// const [quote, setQuote] = useState([]);
+	// const [data, setData] = useState([]);
 
-			await fetch(
-				`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
-			)
-				.then((res) => res.json())
-				.then((result) => {
-					setData(result);
-					console.log(result);
-				});
-		};
-		fetchData();
-	}, [lat, long, quote]);
+	// useEffect(() => {
+	// 	const fetchData = async () => {
+	// 		navigator.geolocation.getCurrentPosition(function (position) {
+	// 			setLat(position.coords.latitude);
+	// 			setLong(position.coords.longitude);
+	// 		});
+
+	// 		// await fetch('https://quotes.rest/qod')
+	// 		// 	.then((res) => res.json())
+	// 		// 	.then((resultQuote) => {
+	// 		// 		setData(resultQuote);
+	// 		// 		console.log(resultQuote);
+	// 		// 	});
+
+	// 		await fetch(
+	// 			`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+	// 		)
+	// 			.then((res) => res.json())
+	// 			.then((result) => {
+	// 				setData(result);
+	// 				console.log(result);
+	// 			});
+	// 	};
+	// 	fetchData();
+	// }, [lat, long]);
 
 	return (
 		<div className='home'>
 			<div className='homer-header'>
 				<h1>welcome to the internet {name}</h1>
+				{/* <button onClick={handleClick}>Click me</button> */}
 				<div className='weather-container'>
-					{typeof data.main != 'undefined' ? (
+					{/* {typeof data.main != 'undefined' ? (
 						<Weather weatherData={data} />
 					) : (
 						<div></div>
-					)}
+					)} */}
 				</div>
 			</div>
-			<button onClick={handleClick}>Click me</button>
 
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
-				sed temporibus quo ex suscipit, quam tempora exercitationem ab
-				deserunt id, ipsum ut, voluptatum similique voluptates dolore
-				debitis fugit? Exercitationem, saepe.
-			</p>
+				<Search />
 		</div>
 	);
 };
