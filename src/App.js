cd3deb0716cc69ac3d0439d86d6378608c1130c2
Tyler Koenig <tylerkng96@icloud.com>
2021-10-15 23:24:32 -0400

    accessing page by refresh

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import Footer from './components/Footer';
 import HomePage from './components/pages/HomePage';
 import ContactPage from './components/pages/ContactPage';
 import { ErrorPage } from './components/pages/ErrorPage';
-import {
-	BrowserRouter as Router,
-	Route,
-	Switch,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import car from './images/bg.jpg';
 
 export default function App() {
+	const [bgImg, setBgImg] = useState(`${car}`);
+
+
+
+	const pageAccessedByReload =
+		(window.performance && window.performance.type === 1) ||
+		window.performance
+			.getEntriesByType('navigation')
+			.map((nav) => nav.type)
+			.includes('reload');
+
+	useEffect(() => {
+		alert(pageAccessedByReload);
+		// setBgImg()
+	}, [pageAccessedByReload]);
+
 	return (
 		<Router>
-			<div className='wrapper'>
+			<div
+				className='wrapper'
+				style={{ backgroundImage: `url(${bgImg})` }}
+			>
 				<div className='content'>
 					<Switch>
 						<Route exact path='/' component={HomePage} />
